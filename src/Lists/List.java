f24df719c0c15366ca31b3286208c86e69c02ecb
marisolalfaro <marialfarolo@est.unt.ac.cr>
2025-06-26 13:29:10 -0600

    Se crea la clase Lists
    Se crea la interface List con el método add

--- src/Lists/List.java
+++ src/Lists/List.java
@@ -1,18 +1,7 @@
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package Lists;
-
-/**
- *
- * @author Ricardo Chaves
- */
 public interface List <T> {
     
     public T find(Object id);
-
-
     public boolean remove(T t);
+    public boolean add(T t);
 }
