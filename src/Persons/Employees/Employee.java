595cedfebe88bc8883b6f1e98d6e54c56d607de3
Greivin Arguedas <greivin.arguegud@gmail.com>
2025-06-28 21:49:37 -0600

    Actualizacion Clase Employees
    Parcheo de error presente en la clase, ya solucionado 100%

--- src/Persons/Employees/Employee.java
+++ src/Persons/Employees/Employee.java
@@ -1,22 +1,14 @@
+
 package Persons.Employees;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 
 import Persons.Person;
 import Utils.UtilDate;
 import java.time.LocalDate;
-import java.time.Period;
 
-/**
- *
- * @author Usuario
- */
-class Employee {
-      public Double getSalary() {
+public abstract class Employee extends Person {
+    private Double salary;
+
+     public Double getSalary() {
         return salary;
       }
         
@@ -25,7 +17,7 @@ class Employee {
             this.salary = salary;
     }
     private static boolean validateSalary(double salary){
-        return salary>=3000;
+        return salary >= 3000;
     }
     
     public Employee(String id, String name, LocalDate birthDate, String phone, Double salary) {
@@ -36,6 +28,5 @@ class Employee {
     @Override
     public String toString() {
         return "id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", phone=" + phone + ", salary=" + salary;
-
-}
+    }
 }
